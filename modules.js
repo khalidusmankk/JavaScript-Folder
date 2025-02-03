1a8f343af75e90f7199a9947b1f9f09aed169b4b
Khalid Usman <khalidusmankkpk@gmail.com>
2025-02-03 08:52:19 +0500

    done

--- modules.js
+++ modules.js
@@ -1,3 +1,7 @@
+//JavaScript modules allow you to break up your code into separate files.
+//This makes it easier to maintain the code-base.
+//ES Modules rely on the import and export statements.
+
 function tryModules() {
   let name = "Khalid";
   let age = 35;
